a5425f497973ece11449eb3626479c621961c84d
saltire sable <saltiresable@gmail.com>
2019-06-02 20:10:54 -0400

    Mobs following mouse cursor

---
+++ Assets/Scripts/FollowScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class FollowScript : MonoBehaviour {
+    public Transform target;
+
+    NavMeshAgent agent;
+
+    void Start() {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update() {
+        agent.destination = target.position;
+    }
+}
---
+++ Assets/Scripts/TargetScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetScript : MonoBehaviour {
+    public float planeHeight = 0;
+
+    Camera cam;
+    Plane plane;
+
+    void Start() {
+        cam = FindObjectOfType<Camera>();
+        plane = new Plane(Vector3.up, planeHeight);
+    }
+
+    void Update() {
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        float enter;
+        plane.Raycast(ray, out enter);
+        transform.position = ray.GetPoint(enter);
+    }
+}
